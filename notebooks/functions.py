085ad13055ca91ffb9e9a99d49321f958e273de4
Henry Soysa <henry.soysa@gmail.com>
2023-12-22 15:04:22 +0100

    adding final version of notebooks to see if PR is successful

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as st
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, cohen_kappa_score
 
 
-# define function: checking degree of normal distribution for numerical columns to consider 
+# define function: checking degree of normal distribution for numerical columns to consider
 # when looking for significant difference between various population means
 
 def normaltest_mult_col(df: pd.DataFrame) -> str:
     '''
-    This function, normaltest_mult_col(df), performs a normality test on each numerical column in a DataFrame df. 
-    It selects numerical columns, performs a normality test on each, and prints the results. 
-    Input: DataFrame 
+    This function, normaltest_mult_col(df), performs a normality test on each numerical column in a DataFrame df.
+    It selects numerical columns, performs a normality test on each, and prints the results.
+    Input: DataFrame
     Output: string.
-    
+
     '''
     df_num = df.select_dtypes(np.number)
 
@@ -24,14 +25,14 @@ def normaltest_mult_col(df: pd.DataFrame) -> str:
             print(f'{col}:', statistic, (pvalue))
     print('/n')
 
-    
-# Function for looking for significant difference between two populations' means. 
+
+# Function for looking for significant difference between two populations' means.
 def ttest_ind_2(df1: pd.DataFrame, y: str, y_val, y_val2, random_state: int, significance_level: float=0.05):
     """
-    Function: Split dataframes based on input in y column (binary) and compare statistical difference between each numerical column of split dataframe. 
-    This is is done in order to identify potential X candidates for a logistic regression model to predict y outcomes. 
+    Function: Split dataframes based on input in y column (binary) and compare statistical difference between each numerical column of split dataframe.
+    This is is done in order to identify potential X candidates for a logistic regression model to predict y outcomes.
     Inputs: dataframe, name of y column, potential y value 1, potential y value 2, random state for scipy.stats.ttest_ind()
-    Outputs: List of column names from dataframe that fall into the rejection zones and can be stated have means which are significantly different 
+    Outputs: List of column names from dataframe that fall into the rejection zones and can be stated have means which are significantly different
     from one another across y_value split
     """
     df1_num = df1.loc[df1[y] == y_val].select_dtypes(np.number)
@@ -42,8 +43,8 @@ def ttest_ind_2(df1: pd.DataFrame, y: str, y_val, y_val2, random_state: int, sig
             print(col, stat, pval)
         elif pval < (significance_level/2):
             print(col, stat, pval)
-            
-            
+
+
 # define function to check selected columns'. skewness
 
 def col_skewness(df, columns):
@@ -56,6 +57,46 @@ def col_skewness(df, columns):
         else:
             print(f'{col} skewness, negative skewed: ',skewness)
 
-if __name__ == “__main__”:
-			function1()
-			function2()
+
+from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error, mean_squared_error, r2_score
+
+
+# Create function to store metrics reports from model predictions
+import pandas as pd
+
+def class_error_metrics_report_m(model_1_name: str, y_test_1: list, y_test_pred_1: list, model_2_name: str = None, y_test_2: list = None, y_test_pred_2: list = None) -> pd.DataFrame:
+    '''
+    Function: Calculate the various error metrics for a given set of y_test and y_test_pred splits
+    and organises them into a DataFrame for easier visualization
+    Inputs: model name: str, y_test: list, y_test_pred: list for two models (2nd optional) to be compared
+    Outputs: DataFrame with metrics columns
+    '''
+    # Model 1 metrics
+    error_metrics = {
+        'Model Name': [model_1_name],
+        'Accuracy': [accuracy_score(y_test_1, y_test_pred_1)],
+        'Precision': [precision_score(y_test_1, y_test_pred_1, zero_division=0)],
+        'Recall': [recall_score(y_test_1, y_test_pred_1)],
+        'F1': [f1_score(y_test_1, y_test_pred_1)],
+        'Kappa': [cohen_kappa_score(y_test_1, y_test_pred_1)]
+    }
+
+    # Model 2 Metrics if provided
+    if model_2_name is not None:
+        # Calculate metrics for model 2
+        error_metrics['Model Name'].append(model_2_name)
+        error_metrics['Accuracy'].append(accuracy_score(y_test_2, y_test_pred_2))
+        error_metrics['Precision'].append(precision_score(y_test_2, y_test_pred_2, zero_division=0))
+        error_metrics['Recall'].append(recall_score(y_test_2, y_test_pred_2))
+        error_metrics['F1'].append(f1_score(y_test_2, y_test_pred_2))
+        error_metrics['Kappa'].append(cohen_kappa_score(y_test_2, y_test_pred_2))
+
+    # Create the report DataFrame
+    report = pd.DataFrame(error_metrics)
+    return report
+
+if __name__ == "__main__":
+            normaltest_mult_col()
+            ttest_ind_2()
+            col_skewness()
+            class_error_metrics_report_m()
